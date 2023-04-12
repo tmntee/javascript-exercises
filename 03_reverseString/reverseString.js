b56d5588f0f71bda6f570687edda03ef1b7f354c
tmntee <107428229+tmntee@users.noreply.github.com>
2023-04-12 15:09:32 -0400

    Tweak javascript code, ran first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,12 @@
 const reverseString = function(string) {
     let word = string;
-    let reversed;
-    for (i = 0; i < word.length; i++)
+    let reversed = "";
+
+    for (let i = word.length; i > -1; i--)
     {
-        reversed = string.pop();
+        reversed += word.charAt(i);
     }
     return reversed;
+};
 // Do not edit below this line
 module.exports = reverseString;
