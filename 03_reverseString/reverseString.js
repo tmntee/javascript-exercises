f657644e1301dad73145204ad2ab1e405b375760
tmntee <107428229+tmntee@users.noreply.github.com>
2023-04-11 22:03:10 -0400

    Write pre-test code for function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let word = string;
+    let reversed;
+    for (i = 0; i < word.length; i++)
+    {
+        reversed = string.pop();
+    }
+    return reversed;
 // Do not edit below this line
 module.exports = reverseString;
