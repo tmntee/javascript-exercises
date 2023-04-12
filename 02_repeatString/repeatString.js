e6c31f8afa8fd1a7e8871ad7c442138fc4f472f7
tmntee <107428229+tmntee@users.noreply.github.com>
2023-04-11 21:15:01 -0400

    Add initial javascript code to exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let word = string;
+    for (let i = 0; i < num; i++)
+    {
+        string =+ string;
+    }
 };
 
 // Do not edit below this line
