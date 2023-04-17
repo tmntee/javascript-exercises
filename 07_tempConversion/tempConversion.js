d9db8a683dc8f9371a50a89b0b624dac456934a7
tmntee <107428229+tmntee@users.noreply.github.com>
2023-04-17 15:53:46 -0400

    Write pre-test code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInFahrenheit) {
+    let celsius = (degreesInFahrenheit - 32) * (5/9);
+    return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInCelsius) {
+  let fahrenheit = (degreesInCelsius * (9/5)) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
