f68f0e1a941943ba3167f867610d3e7f785ab685
tmntee <107428229+tmntee@users.noreply.github.com>
2023-04-17 14:25:39 -0400

    Rewrite pre-test code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
 const removeFromArray = function(array, index) {
-    array.splice(index, 1);
-    return array
+    let newArray = [];
+    for (let i = 0; i < array.length; i++)
+    {
+        if (array[i] === index)
+        {
+            newArray.splice(i, 1)
+        }
+    }
+    return newArray
 };
 
 // Do not edit below this line
