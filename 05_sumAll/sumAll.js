355dd8ed8bb2a152d8f3fcae34f38e6a46e5b03c
tmntee <107428229+tmntee@users.noreply.github.com>
2023-04-17 15:32:26 -0400

    Change code accordingly to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
 const sumAll = function(min, max) {
     let sum = 0;
-    for (let i = max; i >= min; i--)
+    
+    if(typeof(min) != "number" || typeof(max) != "number")
+    {
+        return "ERROR";
+    } 
+    if (min < 0 || max < 0)
+    {
+        return "ERROR";
+    }
+
+    if (min > max)
+    {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+
+    for (let i = min; i <= max; i++)
     {
         sum += i;
     }
